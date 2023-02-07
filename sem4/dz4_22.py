b0a114b7ffa6861f89828ecaa41db64f20b9690a
levchakovd <108088698+levchakovd@users.noreply.github.com>
2023-02-07 01:45:10 -0300

    Update dz4_22.py
    done

--- sem4/dz4_22.py
+++ sem4/dz4_22.py
@@ -7,3 +7,17 @@
 # 2 4 6 8 10 12 10 8 6 4 2
 # 3 6 9 12 15 18
 # 6 12
+from collections import Counter
+
+n = int(input("кол-во эл-тов первого множества: "))
+m = int(input("кол-во эл-тов второго множества: "))
+N = [int(input("Введите число из первого множества: ")) for i in range(n)]
+M = [int(input("Введите число из второго множества: ")) for i in range(m)]
+
+def intersection(list1, list2):
+    c1, c2 = Counter(list1), Counter(list2)
+    common_elements = set(c1.keys()) & set(c2.keys())
+    return sorted(list(common_elements))
+print(N)
+print(M)
+print(intersection(N,M))
