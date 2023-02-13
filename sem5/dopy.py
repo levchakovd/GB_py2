02ba2c30cb49a91d0298ed1ec494f5e1441760b9
levchakovd <108088698+levchakovd@users.noreply.github.com>
2023-02-12 22:26:28 -0300

    Create dopy.py
    codewars

---
+++ sem5/dopy.py
@@ -0,0 +1,40 @@
+
+# def solution(text, ending):
+#     if ending in (text-text[0]):
+#         return
+#     pass
+
+# a = input(": ")
+# b = input(":  ")
+
+# print(solution(a,b))
+
+# def find_outlier(integers):
+#     for i in range(len(integers)):
+#         if integers[i]%2 == 0:
+#             return integers[i] != 0
+#         if integers[i]%2 != 0:
+#             return integers[i]%2 == 0
+
+# def get_sum(a, b):
+#     if a == b:
+#         return a
+#     elif a < b:
+#         return sum(range(a, b + 1))
+#     else:
+#         return sum(range(b, a + 1))
+
+# a = int(input(": "))
+# b = int(input(":  "))
+
+# print(get_sum(a,b))
+
+# def square_digits(n):
+#     return int(''.join([str(int(i)**2) for i in str(n)]))
+
+# num = int(input(": "))
+# print(square_digits(num))    
+
+def friend(x):
+    return [friend for friend in x if len(friend) == 4]
+
