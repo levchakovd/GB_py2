e0cde97746d6531621c267a5884b9b4994d641e3
levchakovd <108088698+levchakovd@users.noreply.github.com>
2023-02-18 04:35:32 -0300

    Update dz7_36.py
    done

--- sem7/dz7_36.py
+++ sem7/dz7_36.py
@@ -5,9 +5,29 @@
 # почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
 # ровно два аргумента, как, например, у операции умножения.
 # Ввод: Вывод:
-# print_operation_table(lambda x, y: x * y) 1 2 3 4 5 6
+# print_operation_table(lambda x, y: x * y) 
+#  1 2 3 4 5 6
 #  2 4 6 8 10 12
 #  3 6 9 12 15 18
 #  4 8 12 16 20 24
 #  5 10 15 20 25 30
 #  6 12 18 24 30 36 
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    # Выводим заголовок таблицы
+    print(" ", end="\t")
+    for j in range(1, num_columns+1):
+        print(j, end="\t")
+    print()
+    
+    # Выводим содержимое таблицы
+    for i in range(1, num_rows+1):
+        print(i, end="\t")
+        for j in range(1, num_columns+1):
+            value = operation(i, j)
+            print(value, end="\t")
+        print()
+
+print_operation_table(lambda x, y: x * y)
+
+
