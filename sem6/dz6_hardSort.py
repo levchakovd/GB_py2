89853a6ef79735adf6bc232e4b994c7275edf3d2
levchakovd <108088698+levchakovd@users.noreply.github.com>
2023-02-18 02:47:37 -0300

    Update dz6_hardSort.py
    done

--- sem6/dz6_hardSort.py
+++ sem6/dz6_hardSort.py
@@ -9,3 +9,31 @@
 # После сортировки
 # 1 2 3 4
 # 5 7 9 10
+import random
+rows = int(input("Введите количество строк: "))
+cols = int(input("Введите количество столбцов: "))
+
+# Инициализируем двумерный массив 
+matrix = [[random.randint(0,10) for j in range(cols)] for i in range(rows)]
+
+# Выводим массив на экран
+for i in range(rows):
+    for j in range(cols):
+        print(matrix[i][j], end=' ')
+    print()
+
+print("-"*cols)
+#объеденяем все в один список
+flat_matrix = [num for row in matrix for num in row]
+
+#сортируем полученный список
+flat_matrix.sort()
+
+#собираем мартицу отсортированную
+sorted_matrix = [flat_matrix[i:i+cols] for i in range(0, len(flat_matrix), cols)]
+
+# Выводим массив на экран
+for i in range(rows):
+    for j in range(cols):
+        print(sorted_matrix[i][j], end=' ')
+    print()
