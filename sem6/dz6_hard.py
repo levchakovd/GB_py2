346e99b0d40caa0976693fff847215a0cc4ddf69
levchakovd <108088698+levchakovd@users.noreply.github.com>
2023-02-18 03:29:22 -0300

    Update dz6_hard.py

--- sem6/dz6_hard.py
+++ sem6/dz6_hard.py
@@ -4,3 +4,44 @@
 # Перемешать случайным образом элементы массива, причем чтобы каждый гарантированно и только один раз 
 # переместился на другое место и выполнить это за m*n / 2 итераций. То есть если массив три на четыре, то 
 # надо выполнить не более 6 итераций. И далее в конце опять вывести на экран как таблицу.
+
+import random
+n = int(input("Введите количество строк: "))
+m = int(input("Введите количество столбцов: "))
+if n*m %2 != 0:
+    while (n*m %2 != 0):
+        print("так не получится, давайте сделаем так, чтобы кол-во эл-тов было четным!")
+        m = int(input("Введите количество строк: "))
+        n = int(input("Введите количество столбцов: "))
+
+
+# Инициализируем двумерный массив 
+matrix = [[random.randint(0,10) for j in range(m)] for i in range(n)]
+
+# Выводим массив на экран
+for i in range(n):
+    for j in range(m):
+        print(matrix[i][j], end=' ')
+    print()
+
+print("-"*m)
+
+flat_matrix = [num for row in matrix for num in row]
+#print(flat_matrix)
+
+#выглядит как костыль, но:
+randrange = list(range(0,len(flat_matrix)))
+random.shuffle(randrange)
+#print(randrange)
+
+count = 0
+
+for i in range(int(m*n/2)):
+    flat_matrix[randrange[i]],flat_matrix[randrange[i+1]] = flat_matrix[randrange[i+1]],flat_matrix[randrange[i]]
+    count +=1
+
+print(f"количество иттераций = {count}")
+
+for i in range(n):
+    for j in range(m):
+        print(flat_matrix[i][j], end = " ")
