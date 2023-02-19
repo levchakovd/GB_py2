9807f9db6407417421814797423f34eeaca26037
levchakovd <108088698+levchakovd@users.noreply.github.com>
2023-02-19 02:21:54 -0300

    Create chatbot.py
    done

---
+++ sem7/chatbot.py
@@ -0,0 +1,32 @@
+from random import *
+import json
+
+teams = []
+teams.append("Кливленд")
+teams.append("Лейкерс")
+teams.append("Портленд")
+teams.append("Атланта")
+teams.append("Денвер")
+
+def save():
+    with open("teams.json","w",encoding="utf-8") as tm:
+        tm.write(json.dumps(teams,ensure_ascii=False))
+    print("Мы добавили команду в список")
+
+while True:
+    command = input("введите команду боту: ")
+    if command == "/start":
+        print("БОТ NBA начал свою работу")
+    elif command == "/stop":
+        print("я перестал работать, увидимся")
+        break
+    elif command == "/all":
+        print("Вот список команд")
+        print(teams)
+    elif command == "/add":
+        t = input("Введите название команды: ")
+        teams.append(t)
+    elif command == "/save":
+        save()
+    else:
+        print("я хз что тебе надо, не умею это")
